9f088a02d4ab08a27b1f6ea4b16cb4d4ca227148
Cem Gökmen <skyman@users.noreply.github.com>
2017-03-26 22:39:21 -0400

    Update nin10kit.rb

--- Formula/nin10kit.rb
+++ Formula/nin10kit.rb
@@ -1,7 +1,7 @@
 class Nin10kit < Formula
   desc "A set of tools for doing homebrew game development, includes look up table generators and and image to C exporter for Nintendo's Gameboy Advance, DS, and 3DS systems, used in Georgia Tech CS2261."
   homepage "https://github.com/TricksterGuy/nin10kit"
-  version "0.2"
+  version "0.3"
   url "https://github.com/TricksterGuy/nin10kit.git", :using => :git, :revision => "f5d67bf968963e7cd4699cae8007d5bdc873fc7c"
 
   # This pulls from the master branch instead of the specified version tag
