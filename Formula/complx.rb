89ae65598693ccd1b929e09565f46e81c0adc5b9
Cem Gökmen <skyman@users.noreply.github.com>
2018-01-13 17:20:47 -0500

    Updated complx to 4.16.0

--- Formula/complx.rb
+++ Formula/complx.rb
@@ -1,8 +1,8 @@
 class Complx < Formula
   desc "The LC-3 Simulator used in Georgia Tech CS2110."
   homepage "https://github.com/TricksterGuy/complx"
-  version "4.15.6"
-  url "https://github.com/TricksterGuy/complx.git", :using => :git, :tag => "4.15.6"
+  version "4.16.0"
+  url "https://github.com/TricksterGuy/complx.git", :using => :git, :tag => "4.16.0"
 
   # This pulls from the master branch instead of the specified version tag
   head do
